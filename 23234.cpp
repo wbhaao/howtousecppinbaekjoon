0bb4a93d932249f8f25c05bd7ce16b822bbe02ff
김규민 <82032508+wbhaao@users.noreply.github.com>
2024-02-28 15:42:26 +0900

    7891

--- 23234.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "The world says hello!";
-    return 0;
-}
---
+++ 7891.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int a;
+    std::cin >> a;
+    for (int i = 0; i < a; i++) {
+        int b = 0;
+        int c = 0;
+        std::cin >> b >> c;
+        std::cout << b+c << std::endl;
+    }
+    return 0;
+}
